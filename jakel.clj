7f8ca1cd93184d25d16b7411eb5f6ec21d999e1f
Jacob Emcken <jacob@emcken.dk>
2024-12-21 23:50:36 +0100

    Add initial config reading

--- jakel.clj
+++ jakel.clj
@@ -1,6 +1,7 @@
 #!/usr/bin/env bb
 (ns jakel
-  (:require [clojure.tools.cli :as cli]))
+  (:require [clj-yaml.core :as yaml]
+            [clojure.tools.cli :as cli]))
 
 (def cli-options
   [["-s" "--source DIR" "Directory where files are read from." :default "./"]
@@ -12,13 +13,21 @@
   []
   (str "Usage: jakel build -d <dir>"))
 
+(defn read-config
+  [file]
+  (-> file
+      slurp
+      yaml/parse-string))
+
 (defn main
   [& args]
-  (let [{:keys [_options arguments _summary]} (cli/parse-opts args cli-options)
+  (let [{:keys [options arguments _summary]} (cli/parse-opts args cli-options)
         [command] arguments]
     (when (not= "build" command)
       (println (usage))
       (System/exit 2))
-    (println "building!")))
+
+    (println "building!")
+    (println (read-config (str (:source options) (:config options))))))
 
 (apply main *command-line-args*)
